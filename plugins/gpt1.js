e0ea5b29677a343e28d6d8c313de8337aa1ae7d5
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-12-07 11:25:07 +0300

    Update gpt1.js

--- plugins/gpt1.js
+++ plugins/gpt1.js
@@ -31,7 +31,7 @@ const contextInfo = {
   }
 };
 
-popkid({ nomCom: "gpt", reaction: "🤦", categorie: "bmbai" }, async (dest, zk, commandeOptions) => {
+popkid({ nomCom: "gpt1", reaction: "🤦", categorie: "bmbai" }, async (dest, zk, commandeOptions) => {
   const { repondre, arg, ms } = commandeOptions;
 
   try {
